e48fa1d66421da35377ea29aa320decb617c0619
marcelo <marartb4@hotmail.com>
2017-08-18 22:37:20 +0200

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,37 @@
+ien continuamos son "c".
+
+En este tuto vamos a declarar dos variables y sumarlas para mostrarlas por pantalla.
+
+aqui el codigo.
+
+//programa que suma dos varibles(marcelo)
+
+#include<stdio.h>
+//llamamos ala funcion main
+
+int main(){
+//declaramos las variables x e y.
+int x;
+int y;
+
+//les damos valores
+x=20;
+y=10;
+//las sumamos
+x+=y;
+//mostramos resultado en pantalla
+printf("El resultado de la suma de x+y es: %i ",x);
+
+//fin del programa
+return(0);
+}
+
+
+
+
+***********************************************************************************************
+Aclaracion en la suma de las variables:
+(x+=y;) tambien podemos hacer x=x+y;
+es lo mismo.
+
+**********************************************************************************************************
