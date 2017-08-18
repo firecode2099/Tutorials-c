ecbb0cd02e06d7343566dde2a6bb8874cf4f79bb
marcelo <marartb4@hotmail.com>
2017-08-18 22:18:57 +0200

    first Hello wrold on c! for beginers español
    i did this tutorial in the past wen i was leraning.
    Tutorial de antaño en mis primeros pasos con c.

---
+++ tuto1.c
@@ -0,0 +1,69 @@
+/*
+Bueno primero vamos hace a sacar un "hola mundo" por pantalla.
+
+Primero deben instalar (codeblock) o (devc++)> aqui el link:http://www.codeblocks.org/downloads/26
+para los que tienen win32 bits es la x86.
+
+Deben tambien tener instalado minw -gcc++:aqui el link:https://sourceforge.net/projects/mingw/files/
+
+despues de tener todo instalado comensamos!.
+
+paso-1 abrimos codeblock !
+paso -2 File new/Empyfile
+paso3- le damos save
+paso-4 Nos abrira una vantana, ojo! poner cualquier nombre .c"
+pueden poner cualquier nombre,pero siempre con extencion.c"
+
+luego de salvar el codigo , si no lo entieden no se preocupen , lo explico despues.
+
+/*tutorial basico c
+marcelo ,para github foro!*/
+
+//primero las librerias estandar
+#include <stdio.h>
+
+//la funcion main
+int main(){
+
+//sacamos mensaje por pantalla
+printf("hola mundo!\n");
+
+
+//fin del programa
+return(0);
+
+}
+/*
+le dan f9 y saldra esto por pantalla:
+***************************************************************************************************************************
+hola mundo!
+********************************************************************
+********aclaracion>las barras>> "//" dobles significan comentarios ,asi que codeblock no las compilara
+
+solo lo que esta debajo de los cometarios es codigo ,que se ejecutara.
+y recuerden simpre despues de cualquier parrafo poner (;) siempre!
+
+-La funcion main es la funcion principal , todo debe ir dentro de esta funcion. hay algunas exepciones ,pero ya las veremos,mas adelante.
+la sintaxis es la siguiente:
+
+int main(){
+
+//cuerpo de la funcion
+aqui se debe colocar el codigo!
+
+}//corchetes que nos dicen donde ,empieza y termina la funcion!
+esto es el programa mas basico que se puede escribir en c.
+cualquier duda consulten,saludos
+
+marcelo
+
+
+Por MARCPP
+
+
+
+
+
+
+*/
+
