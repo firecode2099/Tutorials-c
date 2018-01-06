965615011a477c2cd56c5f55af98aff9be1e5dee
marcelo <marartb4@hotmail.com>
2018-01-06 19:12:57 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,28 @@
+/*
+
+Prueba c++
+
+*/
+
+#include<iostream>
+
+
+
+using namespace std;
+
+int main(){
+
+
+string nombre;
+
+cin>>nombre;
+
+cout<<"hola al mejor !"<<nombre<<"\n";
+
+
+
+
+
+return 0;
+
+}
